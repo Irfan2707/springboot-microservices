3f7a9b8fe20024d09194ab283c58eb4df30640d2
irfan <irfanhusain2707@gmail.com>
2025-07-06 23:03:11 +0530

    feat: add API gateway server with routing configuration and update product service to use JPA

--- product-service/src/main/java/com/shopify/product/model/Product.java
+++ product-service/src/main/java/com/shopify/product/model/Product.java
@@ -1,22 +1,27 @@
 package com.shopify.product.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 
-@Document(value = "product")
+@Entity
+@Table(name = "product")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 public class Product {
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String description;
     private BigDecimal price;
--- product-service/src/main/java/com/shopify/product/repository/ProductRepository.java
+++ product-service/src/main/java/com/shopify/product/repository/ProductRepository.java
@@ -1,11 +1,11 @@
 package com.shopify.product.repository;
 
 import com.shopify.product.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends MongoRepository<Product, String> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 
 }
